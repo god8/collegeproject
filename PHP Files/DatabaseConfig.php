a8693098e66af0a96b0d795a204ede1c1931023e
god8 <aggarwalyash309@gmail.com>
2018-07-19 10:36:48 +0530

    First commit

---
+++ PHP Files/DatabaseConfig.php
@@ -0,0 +1,15 @@
+<?php
+
+//Define your host here.
+$HostName = "localhost";
+
+//Define your database username here.
+$HostUser = "root";
+
+//Define your database password here.
+$HostPass = "6fOe3jRjlasngZin";
+
+//Define your database name here.
+$DatabaseName = "employee101";
+
+?>
